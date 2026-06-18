756c9c4dfb451ca3ad2a5fa38b93556d77b28c7c
holloweyed <Galuzavioletta-2005@mail.ru>
2026-06-18 19:04:34 +0300

    fix psycopg

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,51 +1,37 @@
-import pytest
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
 from sqlalchemy import text
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import AsyncSessionLocal, init_db as create_tables
 from src.main import app
 from src.models import User
 
-TEST_DATABASE_URL = "postgresql+asyncpg://kubsu:kubsu@127.0.0.1:5432/kubsu"
-test_engine = create_async_engine(TEST_DATABASE_URL, echo=True)
-TestAsyncSessionLocal = sessionmaker(
-    bind=test_engine, 
-    class_=AsyncSession, 
-    expire_on_commit=False
-)
-
 
 @pytest_asyncio.fixture(scope="session")
 async def init_db() -> None:
-    async with test_engine.begin() as conn:
-        await conn.run_sync(User.metadata.create_all)
-    yield
-    async with test_engine.begin() as conn:
-        await conn.run_sync(User.metadata.drop_all)
+    await create_tables()
 
 
 @pytest_asyncio.fixture(scope='function')
 async def db() -> AsyncSession:
-    async with TestAsyncSessionLocal() as session:
+    async with AsyncSessionLocal() as session:
         yield session
 
 
-@pytest_asyncio.fixture(scope='function')
+@pytest_asyncio.fixture(autouse=True)
 async def test_client():
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
         yield client
 
 
-@pytest_asyncio.fixture(autouse=True, scope='function')
+@pytest_asyncio.fixture(autouse=True)
 async def clear_table(init_db, db: AsyncSession) -> None:
-    await db.execute(text("TRUNCATE users RESTART IDENTITY CASCADE;"))
+    await db.execute(text("TRUNCATE users;"))
     await db.commit()
 
 
-@pytest_asyncio.fixture(scope='function')
+@pytest_asyncio.fixture
 async def user(db: AsyncSession) -> User:
     user = User(name="John Doe")
     db.add(user)
